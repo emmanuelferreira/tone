6a3bbc15c10aefa2f3662f3cd1ad1d8d5bd51563
Morgan Fula <morgan.fula@me.com>
2020-08-17 23:06:11 +0200

    adding video bg and new font

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 
 
 module Tone
@@ -17,6 +17,8 @@ module Tone
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
