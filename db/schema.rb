28b477c30f95a0c2d846518ba160fa665662252f
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-08-18 14:48:02 +0200

    style for edit instrument and refacto partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_095802) do
-
+ActiveRecord::Schema.define(version: 2020_08_18_112723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
