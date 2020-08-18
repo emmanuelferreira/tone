a2599b0d33d5db375272374a08ea77623c57cb7c
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-18 18:42:47 +0200

    made some changes on home navabar and dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,11 @@
 class DashboardsController < ApplicationController
   def summary
     if current_user.instruments
-      @instruments = Instrument.where(user: current_user)  
+      @instruments = Instrument.where(user: current_user)
+    end
+
+    if current_user.bookings
+      @bookings = Booking.where(user: current_user)
     end
   end
 end
