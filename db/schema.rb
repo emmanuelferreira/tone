b29609fcd8d3f82fe29d2c2ba68ec9ac3dcb7ff0
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-19 13:36:16 +0200

    added status column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_080858) do
+ActiveRecord::Schema.define(version: 2020_08_19_112407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_080858) do
     t.integer "transfer_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["instrument_id"], name: "index_bookings_on_instrument_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
