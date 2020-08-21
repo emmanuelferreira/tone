3a0664d5074bdec8bac041e4bb8c4cc2f9b541ab
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-21 09:10:57 +0200

    seed update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_112407) do
+ActiveRecord::Schema.define(version: 2020_08_21_064357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_112407) do
     t.string "title"
     t.float "latitude"
     t.float "longitude"
+    t.string "img_url"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@ require "faker"
 
 puts "Cleaning database..."
 User.destroy_all
+# Instrument.destroy_all
+# Booking.destroy_all
+
+
 puts "Creating 1 user..."
 user = User.new(
   username: "emmanuel19",
@@ -17,38 +21,83 @@ user = User.new(
   password: '123456'
 )
   user.save!
-puts "Finished with the user creation!"
+puts "Finished with the user 1 creation!"
 
 
-puts "Cleaning database..."
-Instrument.destroy_all
 puts "Creating instruments..."
-full_address = ["Avenue des Boveresses 42, 1010 Lausanne", "Place de la Palud 2 1002 Lausanne ", "Avenue de la Gare 46 1022 Chavannes-près-Renens"]
-5.times do
-  category = ["guitar", "piano", "flute", "bass"].sample
-  word = ["Rent", "Awesome", "Great"].sample
   instrument = Instrument.new(
     user_id: User.first.id,
-    pickup_address: full_address.sample,
-    category: category,
-    price_per_day: rand(10..60),
-    description: "An ideal #{category} for students and beginners.
-An excellent value in #{category}, Squier's Affinity Series are ideal for players who are just getting started, with solid construction and electronics. This Affinity Series Telecaster provides all the you want from a Tele, along with a smooth-playing Indian laurel fretboard and the ease and convenience of a top-loading bridge. Now available in this limited-edition finish. Case sold separately. Start with a real Telecaster. Order your special-edition Affinity Tele today.",
-    title: "#{word} #{category.capitalize} "
+    pickup_address: "Rue de Lausanne 40, Renens, Vaud, Suisse",
+    category: "guitar",
+    price_per_day: 10,
+    description: "An ideal guitar for students and beginners.
+                  An excellent value in guitar, Squier's Affinity Series are ideal
+                  for players who are just getting started, with solid construction and electronics.
+                  This Affinity Series Telecaster provides all the you want from a Tele,
+                  along with a smooth-playing Indian laurel fretboard and the ease and
+                  convenience of a top-loading bridge. Now available in this limited-edition finish.
+                  Case sold separately. Start with a real Telecaster. Order your special-edition Affinity Tele today.",
+    title: "Best guitar for beginners",
+    img_url: "https://images.unsplash.com/photo-1514649923863-ceaf75b7ec00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
   )
 
   instrument.save!
-end
 
-Booking.destroy_all
 
 booking = Booking.new(
-  instrument_id: Instrument.last.id,
+  instrument_id: Instrument.first.id,
   user_id:  User.last.id,
-  check_in_date: Date.today,
-  check_out_date: Date.today + 3,
-  rental_price_total: 10,
+  check_in_date: Date.today( (-15).day ),
+  check_out_date: Date.today( (-12).day ),
+  rental_price_total: 30,
   status: 'accepted'
 )
-
 booking.save!
+
+booking = Booking.new(
+  instrument_id: Instrument.first.id,
+  user_id:  User.last.id,
+  check_in_date: Date.today + 5,
+  check_out_date: Date.today + 17,
+  rental_price_total: 120,
+  status: 'accepted'
+)
+booking.save!
+
+
+
+puts "Creating 1 user..."
+user = User.new(
+  username: "morgan",
+  email: "morgan@gmail.com",
+  password: '123456'
+)
+  user.save!
+puts "Finished with the user 1 creation!"
+
+
+puts "Creating instruments..."
+  instrument = Instrument.new(
+    user_id: User.first.id,
+    pickup_address: "Rue de Genève, Lausanne, Vaud, Suisse",
+    category: "flute",
+    price_per_day: 20,
+    description: "An ideal flute for students and beginners.
+An excellent value in flute, Squier's Affinity Series are ideal for players who are just getting started, with solid construction and electronics. This Affinity Series Telecaster provides all the you want from a Tele, along with a smooth-playing Indian laurel fretboard and the ease and convenience of a top-loading bridge. Now available in this limited-edition finish. Case sold separately. Start with a real Telecaster. Order your special-edition Affinity Tele today.",
+    title: "Enchanté to rent my awesome flute ",
+    img_url: "https://images.unsplash.com/photo-1567184406952-9f6ba54cb614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  )
+
+  instrument.save!
+
+  booking = Booking.new(
+  instrument_id: Instrument.last.id,
+  user_id:  User.first.id,
+  check_in_date: Date.today + 2,
+  check_out_date: Date.today + 4,
+  rental_price_total: 40,
+  status: 'accepted'
+)
+booking.save!
+
+puts "Done seeding"
